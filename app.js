5aaf72a436b1fff709669fd6673cc2ec162a9faa
varshapanda <varsha.vijayashwini@kalvium.community>
2026-06-18 10:01:28 +0530

    feat: Added a profile greet url, to greet the students

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send(`DevOps assessment app is running on port ${PORT}`);
 });
 
+app.get('/profile', (req, res)=>{
+  res.send(`<h1>Hi student!, this is your default profile page<h1>`)
+})
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 
-const PORT = Number(process.env.PORT) || 5000;
+// const PORT = Number(process.env.PORT) || 5000;
 
 const PORT = Number(process.env.PORT) || 3000; // default port for dev
-
 
 module.exports = {
   PORT,
